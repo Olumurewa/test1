68116d0586f6926d71a6ff9f1c5a27bb0dfb8ad2
Adegun Olumurewa <olumurewaadegun@gmail.com>
2022-08-24 08:42:29 +0100

    created db class instance

--- config/db.php
+++ config/db.php
@@ -1,81 +1,108 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-ini_set('error_reporting', E_ALL);
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname="test";
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    //echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+class DbConn {
+    /** 
+     *
+     * Database connection credentials
+     * 
+     * 
+     */
+    protected $servername = "localhost";
+    protected $username = "root";
+    protected $password = "password";
+    protected $dbname="test";
+
+    protected $check = false;
+    protected $result = array();
+
+    /**
+     * Function to initiate database connection
+     * @return true
+     */
+    public function connect($servername,$dbname,$username,$password){
+        if (!$this->check){
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                if($conn){
+                    $this->check = true;
+                    return true;
+                }
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+        }else{
+            return true;
+        }
+    }
+    
 }
 
-function otpGenerate(){
-    $otp = rand(100000,999999);
-    return $otp;
-}
 
-function allUsers(){
-    $servername = "localhost";
-    $username = "root";
-    $password = "password";
-    $dbname="test";
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $stmnt = $conn->prepare('SELECT * FROM `users` ');
-    $stmnt->execute();
 
-    var_dump(json_encode($stmnt->fetchAll(PDO::FETCH_OBJ)));
-    //print_r($stmnt);
-}
+// function otpGenerate(){
+//     $otp = rand(100000,999999);
+//     return $otp;
+// }
 
-function oneUser($id){
-    $servername = "localhost";
-    $username = "root";
-    $password = "password";
-    $dbname="test";
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// function allUsers(){
+//     $servername = "localhost";
+//     $username = "root";
+//     $password = "password";
+//     $dbname="test";
+//     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//     $stmnt = $conn->prepare('SELECT * FROM `users` ');
+//     $stmnt->execute();
 
-    $stmt = $conn->prepare("SELECT * FROM `users` WHERE UserID=?");
-    $stmt->execute([$id]); 
-    $user = $stmt->fetch(PDO::FETCH_OBJ);
-    var_dump(json_encode($user));
-}
+//     var_dump(json_encode($stmnt->fetchAll(PDO::FETCH_OBJ)));
+//     //print_r($stmnt);
+// }
 
-function eUser($email){
-    $servername = "localhost";
-    $username = "root";
-    $password = "password";
-    $dbname="test";
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// function oneUser($id){
+//     $servername = "localhost";
+//     $username = "root";
+//     $password = "password";
+//     $dbname="test";
+//     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-    $stmt = $conn->prepare("SELECT `users.userID` FROM `users` WHERE email = ?");
-    $stmt->execute([$email]); 
-    $user = $stmt->fetch(PDO::FETCH_OBJ);
-    return $user;
+//     $stmt = $conn->prepare("SELECT * FROM `users` WHERE UserID=?");
+//     $stmt->execute([$id]); 
+//     $user = $stmt->fetch(PDO::FETCH_OBJ);
+//     var_dump(json_encode($user));
+// }
 
-    echo $email;
-    echo 'Hello World!';
-}
+// function eUser($email){
+//     $servername = "localhost";
+//     $username = "root";
+//     $password = "password";
+//     $dbname="test";
+//     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-function updateUser($id){
-    $servername = "localhost";
-    $username = "root";
-    $password = "password";
-    $dbname="test";
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//     $stmt = $conn->prepare("SELECT `users.userID` FROM `users` WHERE email = ?");
+//     $stmt->execute([$email]); 
+//     $user = $stmt->fetch(PDO::FETCH_OBJ);
+//     return $user;
 
-    $stmt = $conn->prepare("UPDATE`users` SET email=?, password=?, isVerrified=? WHERE UserID=?");
-    try {
-        $conn->beginTransaction();
-            $stmt->execute($id);
-        $conn->commit();
-    }catch (Exception $e){
-        $conn->rollback();
-        throw $e;
-}
-}
+//     echo $email;
+//     echo 'Hello World!';
+// }
+
+// function updateUser($id){
+//     $servername = "localhost";
+//     $username = "root";
+//     $password = "password";
+//     $dbname="test";
+//     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+//     $stmt = $conn->prepare("UPDATE`users` SET email=?, password=?, isVerrified=? WHERE UserID=?");
+//     try {
+//         $conn->beginTransaction();
+//             $stmt->execute($id);
+//         $conn->commit();
+//     }catch (Exception $e){
+//         $conn->rollback();
+//         throw $e;
+// }
+// }
 
     
